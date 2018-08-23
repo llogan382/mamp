554565c69dd02282ae999e1d1bee85c8470a28c8
Unknown <luke.logan@northstarmarketing.com>
2018-08-23 13:45:09 -0400

    USes try catch to display errors

--- inc/connection.php
+++ inc/connection.php
@@ -1,12 +1,14 @@
 <?php
 
-try {$db = new PDO( "sqlite:".__DIR__."/database.db"); //Tries to create an object w the PDO object
-var_dump($db);
+try {
+    $db = new PDO( "sqlite:".__DIR__."/database.db"); //Tries to create an object w the PDO object
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) { // if there is a problem, this executes. 
     echo "Unable to connect";//if there is an error, this messaige is displayed.
+    echo $e->getMessage;
     exit;
 }
 
-echo "Unable to connect to the Database";
+echo "Connected to the Database";
 
 ?>
